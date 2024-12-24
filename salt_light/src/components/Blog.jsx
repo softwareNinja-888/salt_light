5e82b6d86ca97cfc907e3669a89392db0d2bc834
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-12-24 11:46:11 +0200

    Add blog section

--- salt_light/src/components/Blog.jsx
+++ salt_light/src/components/Blog.jsx
@@ -1,6 +1,5 @@
 import { Header } from "./Header.jsx";
-import { data, useParams } from "react-router";
-
+import place from '/donations/donateA1.webp'
 import { listingData } from "../data/data.js";
 export function Blog(){
 
@@ -15,7 +14,26 @@ export function Blog(){
             </div>        
             
             <main>
+                {data.map(el=>{
+                   return( <div className="flex flex-col justify-center items-center w-8/12 border border-black rounded-2xl text-sm font-geist  m-auto mt-10 gap-5">
+                        <div className="w-full">
+                            <img src={place} alt='image' className="w-full h-64 rounded-t-2xl" loading="lazy"/>
+                        </div>
 
+                        <div className="flex flex-col gap-2 p-2">
+                            <div className="text-base font-lora self-center">{el.title}</div>
+                            <div className="text-xs px-2 font-nunito">
+                                {el.summary}
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <div className="flex justify-around font-nunito">
+                                    <div className="font-lora">{el.author}</div>
+                                    <div className="">{el.date}</div>
+                                </div> 
+                            </div>
+                        </div>
+                     </div> )
+                })}
             </main>
 
 
