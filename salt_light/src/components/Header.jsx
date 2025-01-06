3c9b33ea020b742d6e6a8b231e23360883f2cfdb
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-06 16:08:17 +0200

    Refactor code

--- salt_light/src/components/Header.jsx
+++ salt_light/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Menu } from "./Menu.jsx"
 import { Link, useLocation, useParams } from "react-router"
 import { useState } from "react"
 import { Button } from "./Button.jsx"
-import menu from '/burgerMenu.webp'
+import menu from '/new/menuWhite.webp'
 
 import { useAuth } from "./AuthContext.jsx"
 
@@ -16,7 +16,7 @@ export function Header(){
 
     // MENU STATE
 
-    const [menuOpen, setMenuOpen] = useState(true);
+    const [menuOpen, setMenuOpen] = useState(false);
     
     const {IsLoggedIn, login, logout,IsOpen,toggleMenu,ActiveUser} = useAuth()
 
@@ -30,8 +30,8 @@ export function Header(){
     return (
         <>
             <div className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-10 text-center">
-                <div className="sm:flex sm:flex-row sm:justify-around">
-                    <div className="flex justify-between px-2 mb-2">
+                <div className="sm:flex sm:flex-row sm:justify-around mb-20">
+                    <div className="flex items-center justify-between px-2 mb-2">
 
                         {/* LOGO */}
                         <div className="flex flex-row-reverse items-center gap-1 cursor-pointer">
@@ -40,22 +40,20 @@ export function Header(){
                         </div>
 
                         {/* BUTTONS */}
-                        <div className="flex gap-2">
 
-                            {IsLoggedIn ? <Button name={ActiveUser} fontSize="sm" page={`/profile/${ActiveUser}`} width="w-32"/> : <>
-                                <Button name="Log in" page="/login"/>
-                                <Button name="Sign Up" page="/signup"/>
-                            </>}
+                        <div className="">
+                            <img  src={menu} onClick={() => setMenuOpen(!menuOpen)} className="w-5" />
+                            {menuOpen && <HamburgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>}
                         </div>
-                    </div>           
+                        
+                    </div>   
 
-                    {/* MENU */}
-                    {/* <Menu IsOpen={IsOpen} toggleMenu={()=>{toggleMenu()}} list={pages.pagesData} axis={'row'}/> */}
-
-                    <div className="">
-                        <img  src={menu} onClick={() => setMenuOpen(!menuOpen)} className="w-5" />
-                        {menuOpen && <HamburgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>}
-                    </div>
+                    {/* <div className="flex gap-2">
+                        {IsLoggedIn ? <Button name={ActiveUser} fontSize="sm" page={`/profile/${ActiveUser}`} width="w-32"/> : <>
+                            <Button name="Log in" page="/login"/>
+                            <Button name="Sign Up" page="/signup"/>
+                        </>}
+                    </div>         */}                    
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <h1 className="text-2xl font-poppins">Connecting Believers, Empowering Ministries</h1>
