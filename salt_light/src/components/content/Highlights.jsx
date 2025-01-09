1f3b4b65141ed25a9def39e519707cf0c24a89db
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-09 11:16:36 +0200

    Style highlight section

--- salt_light/src/components/content/Highlights.jsx
+++ salt_light/src/components/content/Highlights.jsx
@@ -20,22 +20,26 @@ export function Highlights() {
     return (
         <div className="py-10 text-center">
             <h2 className="text-3xl font-bold mb-6">Highlights</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-6 ">
                 {highlights.map((highlight) => (
                     <div
                         key={`${highlight.name || highlight.title}${highlight.id}`}
-                        className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg"
+                        className="bg-gray-100 p-6 rounded-lg shadow-md hover:shadow-lg h-[450px]"
                     >
                         <img
                             src={highlight.imgUrl}
                             alt={highlight.name || highlight.author || highlight.title}
                             className="w-full h-48 object-cover rounded-md mb-4"
                         />
-                        <h3 className="text-xl font-semibold">{highlight.name || highlight.title}</h3>
-                        <p className="text-gray-600">{highlight.price}</p>
-                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600" onClick={()=>{handleNavigation(highlight)}}>
-                            Explore
-                        </button>
+                        <h3 className="text-xl font-poppins">{highlight.name || highlight.title}</h3>
+                        <p className="text-gray-600 mt-2 text-start font-roboto">{highlight.description || highlight.mission || highlight.summary}</p>
+                        <div className="flex justify-center gap-10 mt-3">
+                            <button className="font-nunito mt-4 px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-8/12" onClick={()=>{handleNavigation(highlight)}}>
+                                Explore
+                            </button>
+                          
+                        </div>
+
                     </div>
                 ))}
             </div>
